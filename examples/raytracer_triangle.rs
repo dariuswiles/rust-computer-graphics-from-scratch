00dc38d89ad54d94655e725af3a37c8f1ad6cda2
Darius Wiles <github@dariuswiles.com>
2021-05-14 14:53:46 -0700

    Work in progress - debug displays triangle

--- examples/raytracer_triangle.rs
+++ examples/raytracer_triangle.rs
@@ -4,7 +4,7 @@
 //! - the ability to set an arbitrary position and direction for the camera; and
 //! - triangle entities
 //! as discussed in various sections of chapter 5.
-//!
+
 
 use rust_computer_graphics_from_scratch::canvas::{Canvas, Rgb};
 #[allow(unused_imports)]
@@ -28,6 +28,7 @@ struct Scene {
 }
 
 #[derive(Clone, Copy, Debug)]
+#[allow(dead_code)]
 enum SceneEntity {
     Light(LightType),
     Sphere(SphereEntity),
@@ -103,7 +104,6 @@ impl TriangleEntity {
 
         Self { corners: corners, plane_point: corners[0].clone(), plane_normal: normal,
                color: color, specular: specular, reflective: reflective }
-
     }
 }
 
@@ -114,20 +114,6 @@ impl TriangleEntity {
 ///
 /// A `Result::Err` is returned if the line and plane are parallel. This includes the case where
 /// the line is entirely within the plane.
-///
-/// # Examples
-/// ```
-/// let plane_point = Vector3::new(0.0, 0.0, 1.0);
-/// let plane_normal = Vector3::new(1.0, 0.0, -1.0);
-///
-/// let line_point = Vector3::new(3.0, 1.0, -2.0);
-/// let line_direction = Vector3::new(0.0, 0.0, 1.0);
-///
-/// let intersect_point = intersect_line_and_plane(&plane_point, &plane_normal, &line_point,
-///                                                &line_direction);
-///
-/// assert_eq(intersect_point, Vector3::new(3.0, 1.0, 4.0));
-/// ```
 fn intersect_line_and_plane(plane_point: &Vector3, normal: &Vector3, line_point: &Vector3,
                             direction: &Vector3) -> Result<Vector3, ()> {
 
@@ -239,11 +225,12 @@ fn compute_lighting(
                     t_max = f64::INFINITY;
                 }
 
-                // Shadow check - do not add light from this source if there is a sphere
+                // Shadow check - do not add light from this source if there is a entity
                 // intersecting it
-                let (shadow_sphere, _shadow_t) = closest_intersection(&position, &light_vector,
+                let (shadow_entity, _shadow_t) = closest_intersection(&position, &light_vector,
                                                                       0.001, t_max, &scene);
-                if shadow_sphere != Option::None{
+
+                if let Some(_) = shadow_entity {
                     continue;
                 }
 
@@ -277,36 +264,56 @@ fn compute_lighting(
 }
 
 
-/// Returns the handle of the closest sphere and the distance of its closest intersection from the
+/// Returns the handle of the closest entity and the distance of its closest intersection from the
 /// `origin` provided. The intersection is determined by extending the `direction` vector provided
 /// from this `origin`. Only intersections within the range `t_min` and `t_max`, which are measured
-/// in world units, are considered. If no sphere intersects, the sphere handle returned is
+/// in world units, are considered. If no entity intersects, the entity returned is
 /// `Option::None`, and the distance of closest intersection is `f64::INFINITY`.
-fn closest_intersection<'a>(origin: &Vector3,
+fn closest_intersection(origin: &Vector3,
                         direction: &Vector3,
                         t_min: f64,
                         t_max: f64,
-                        scene: &'a Scene)
-                        -> (Option<&'a SphereEntity>, f64) {
+                        scene: &Scene)
+                        -> (Option<SceneEntity>, f64) {
+
     let mut closest_t = f64::INFINITY;
-    let mut closest_sphere = Option::<&SphereEntity>::None;
+    let mut closest_entity = Option::<SceneEntity>::None;
 
     for scene_ent in scene.entities.iter() {
-        if let SceneEntity::Sphere(s) = scene_ent {
-            let (t1, t2) = intersect_ray_sphere(origin, direction, s);
-            if (t_min < t1) & (t1 < t_max) & (t1 < closest_t) {
-                closest_t = t1;
-                closest_sphere = Some(&s);
-            }
-            if (t_min < t2) & (t2 < t_max) & (t2 < closest_t) {
-                closest_t = t2;
-                closest_sphere = Some(&s);
-            }
+        match scene_ent {
+            SceneEntity::Sphere(s) => {
+                let (t1, t2) = intersect_ray_sphere(origin, direction, s);
+                if (t_min < t1) & (t1 < t_max) & (t1 < closest_t) {
+                    closest_t = t1;
+                    closest_entity = Some(SceneEntity::Sphere(*s));
+                }
+                if (t_min < t2) & (t2 < t_max) & (t2 < closest_t) {
+                    closest_t = t2;
+                    closest_entity = Some(SceneEntity::Sphere(*s));
+                }
+
+            },
+            SceneEntity::Triangle(t) => {
+
+                let ray_intersect_t = intersect_line_and_plane(&t.plane_point, &t.plane_normal,
+                                                               origin, direction);
+
+                if let Ok(tri) = ray_intersect_t {
+                    if is_point_inside_triangle(&tri, &t.corners[0], &t.corners[1],
+                                                &t.corners[2]) {
+                        closest_t = 2.0; // BUG Need to determine correct value for closest_t
+                        closest_entity = Some(SceneEntity::Triangle(*t));
+// print!("Found point inside triangle. ");
+                    }
+                }
+            },
+            _ => {}
         }
     }
 
-    return (closest_sphere, closest_t);
+    return (closest_entity, closest_t);
 }
+
 
 /// Returns the color of the closest sphere by extending the `direction` vector provided. Only
 /// considers spheres within the range `t_min` and `t_max`, which are measured in world units. If
@@ -319,32 +326,44 @@ fn trace_ray(origin: &Vector3,
              recursion_depth: i32,
              scene: &Scene,)
              -> Rgb {
-    let (closest_sphere, closest_t) = closest_intersection(&origin, &direction, t_min, t_max, &scene);
+    let (closest_entity, closest_t) = closest_intersection(&origin, &direction, t_min, t_max,
+                                                           &scene);
 
-    if let Some(s) = closest_sphere {
-        let position = origin.add(&direction.multiply_by(closest_t));  // Compute intersection
-        let normal = position.subtract(&s.center);   // Compute normal of sphere at this intersection
-        let normal_norm = normal.normalize().unwrap();  // Panics if light is in same location as sphere surface
-        let intensity = compute_lighting(&position, &normal_norm, &direction.multiply_by(-1.0), s.specular, &scene);
+    let mut ray_color = scene.background_color.clone();  // Default, if ray does not intersect an object
 
-        let local_color = s.color.multiply_by(intensity);
+    if let Some(e) = closest_entity {
+        match e {
+            SceneEntity::Sphere(s) => {
+                let position = origin.add(&direction.multiply_by(closest_t));  // Compute intersection
+                let normal = position.subtract(&s.center);   // Compute normal of sphere at this intersection
+                let normal_norm = normal.normalize().unwrap();  // Panics if light is in same location as sphere surface
+                let intensity = compute_lighting(&position, &normal_norm, &direction.multiply_by(-1.0), s.specular, &scene);
 
-        // If we reach the recursion limit, or the sphere is not reflective, return the local color
-        if (s.reflective <= 0.0) | (recursion_depth <= 0) {
-            return local_color;
-        }
+                let local_color = s.color.multiply_by(intensity);
 
-        // Compute the reflected color
-        let reflected_ray = reflect_ray(&direction.multiply_by(-1.0), &normal_norm);
+                // If we reach the recursion limit, or the sphere is not reflective, return the local color
+                if (s.reflective <= 0.0) | (recursion_depth <= 0) {
+                    return local_color;
+                }
 
-        let reflected_color = trace_ray(&position, &reflected_ray, TRACE_EPSILON, f64::INFINITY, recursion_depth - 1, &scene);
+                // Compute the reflected color
+                let reflected_ray = reflect_ray(&direction.multiply_by(-1.0), &normal_norm);
 
-        return local_color.multiply_by(1.0 - s.reflective).add(&(&reflected_color).multiply_by(s.reflective));
+                let reflected_color = trace_ray(&position, &reflected_ray, TRACE_EPSILON, f64::INFINITY, recursion_depth - 1, &scene);
 
+                ray_color = local_color
+                                .multiply_by(1.0 - s.reflective)
+                                .add(&(&reflected_color)
+                                .multiply_by(s.reflective));
+            },
+            SceneEntity::Triangle(t) => {
+                ray_color = Rgb::from_ints(255, 255, 255);  // TODO Replace this with proper RGB calcs.
 
-    } else {
-        return scene.background_color;
+            },
+            _ => {}
+        };
     }
+    ray_color
 }
 
 
@@ -433,32 +452,44 @@ fn main() {
     let mut canvas = Canvas::new("Chapter 5 Arbitrary camera position", CANVAS_WIDTH,
                                  CANVAS_HEIGHT);
 
-    let scene = create_scene();
-
-    let tri1 = TriangleEntity::new([Vector3::new(2.0, 2.0, 0.0), Vector3::new(6.0, 2.0, 0.0), Vector3::new(6.0, 5.0, 0.0)],
-                                   Rgb::from_ints(0, 255, 0),    // Green
-                                   10,  // Somewhat shiny
-                                   0.4, // Even more reflective
-                                   );
-
-    println!("{:#?}", tri1);
-    panic!("Stop");
-
-
-
-
-
-
-
+    let scene = Scene {
+        viewport_width: VIEWPORT_WIDTH,
+        viewport_height: VIEWPORT_HEIGHT,
+        background_color: Rgb::from_ints(0, 0, 0),     // Black
+        entities: vec!(
+            SceneEntity::Triangle(
+                TriangleEntity::new([Vector3::new(-2.5, 0.5, 6.0), Vector3::new(2.0, 1.0, 5.0), Vector3::new(1.5, 2.0, 5.5)],
+                                    Rgb::from_ints(255, 255, 255),    // White
+                                    10,  // Somewhat shiny
+                                    0.4, // Even more reflective
+                                    )),
+            SceneEntity::Light(LightType::Ambient(AmbientLightEntity{
+                intensity: 0.2,
+            })),
+            SceneEntity::Light(LightType::Point(PointLightEntity{
+                intensity: 0.6,
+                position: Vector3::new(2.0, 1.0, 0.0),
+            })),
+            SceneEntity::Light(LightType::Directional(DirectionalLightEntity{
+                intensity: 0.2,
+                direction: Vector3::new(1.0, 4.0, 4.0),
+            })),
+        )
+    };
 
 
     // Define the position and orientation of the camera
-    let camera_position = Vector3::new(3.0, 0.0, 1.0);
+    let camera_position = Vector3::new(0.0, 0.0, 0.0);
 
     let camera_rotation = Matrix3x3::new(
-                            0.7071, 0.0, -0.7071,
+                            1.0, 0.0, 0.0,
                             0.0,    1.0,  0.0,
-                            0.7071, 0.0,  0.7071);
+                            0.0, 0.0,  1.0);
+
+//     let camera_rotation = Matrix3x3::new(
+//                             0.7071, 0.0, -0.7071,
+//                             0.0,    1.0,  0.0,
+//                             0.7071, 0.0,  0.7071);
 
 
     let cw = CANVAS_WIDTH as i32;
@@ -479,13 +510,14 @@ fn main() {
 }
 
 
+// TODO Find a way to run these tests. Adding "test = true" to Cargo.toml didn't help.
 
 #[cfg(test)]
 fn test_reflect_ray_1() {
     let v1 = Vector3::new(3.0, 7.0, 11.0);
     let v2 = Vector3::new(13.0, 1.0, 5.0);
     let rr_result = reflect_ray(&v1, &v2);
-    assert!(rr_result == Vector3::new(2623.0, 195.0, 999.0));
+    assert_eq!(rr_result, Vector3::new(2623.0, 195.0, 999.0));
 }
 
 #[cfg(test)]
@@ -493,5 +525,19 @@ fn test_reflect_ray_2() {
     let v3 = Vector3::new(3.3, 7.7, 11.1);
     let v4 = Vector3::new(13.3, 1.1, 5.5);
     let rr_result = reflect_ray(&v3, &v4);
-    assert!(rr_result == Vector3::new(3013.406, 241.80202, 1236.41));
+    assert_eq!(rr_result, Vector3::new(3013.406, 241.80202, 1236.41));
+}
+
+#[cfg(test)]
+fn test_intersect_line_and_plane_1() {
+    let plane_point = Vector3::new(0.0, 0.0, 1.0);
+    let plane_normal = Vector3::new(1.0, 0.0, -1.0);
+
+    let line_point = Vector3::new(3.0, 1.0, -2.0);
+    let line_direction = Vector3::new(0.0, 0.0, 1.0);
+
+    let intersect_point = intersect_line_and_plane(&plane_point, &plane_normal, &line_point,
+                                                   &line_direction);
+
+    assert_eq!(intersect_point, Result::Ok(Vector3::new(3.0, 1.0, 4.0)));
 }
