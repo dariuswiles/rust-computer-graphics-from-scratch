6fe7751af5df12068eae3d98236e093cec2d1354
Darius Wiles <github@dariuswiles.com>
2021-05-18 10:22:36 -0700

    Add Point struct and use f64 more

--- examples/raster_01.rs
+++ examples/raster_01.rs
@@ -14,7 +14,24 @@ const CANVAS_WIDTH: usize = 600;
 const CANVAS_HEIGHT: usize = 600;
 
 
-/// Draws a straight line between `x_0`, `y_0` and `x_1`, `y_1` in the given color (inclusive).
+/// A convenience structure to hold a point in 2D space.
+struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Point {
+    #[allow(dead_code)]
+    fn new(x: f64, y: f64) -> Self {
+        Self {x: x, y: y}
+    }
+
+    fn from_ints(x: i32, y: i32) -> Self {
+        Self {x: x as f64, y: y as f64}
+    }
+}
+
+/// Draws a straight line between `p0.x`, `p0.y` and `p1.x`, `p1.y` in the given color (inclusive).
 /// Coordinates outside the canvas coordinates do not result in an error and any part of the line
 /// that is within the canvas will be drawn.
 ///
@@ -33,43 +50,43 @@ const CANVAS_HEIGHT: usize = 600;
 /// canvas.display_until_exit();
 /// ```
 
-fn draw_line(canvas: &mut Canvas, x_0: i32, y_0: i32, x_1: i32, y_1: i32, color: &Rgb) {
-    let x_length = (x_1 - x_0).abs();
-    let y_length = (y_1 - y_0).abs();
+fn draw_line(canvas: &mut Canvas, p0: &Point, p1: &Point, color: &Rgb) {
+    let x_length = (p1.x - p0.x).abs();
+    let y_length = (p1.y - p0.y).abs();
 
     if x_length > y_length {
         let y_start;
         let y_delta;
 
-        if x_0 < x_1 {
-            y_start = y_0 as f64;
-            y_delta = (y_1 - y_0) as f64 / x_length as f64;
+        if p0.x < p1.x {
+            y_start = p0.y as f64;
+            y_delta = (p1.y - p0.y) as f64 / x_length as f64;
         } else {
-            y_start = y_1 as f64;
-            y_delta = (y_0 - y_1) as f64 / x_length as f64;
+            y_start = p1.y as f64;
+            y_delta = (p0.y - p1.y) as f64 / x_length as f64;
         }
 
         let mut y = y_start;
 
-        for x in i32::min(x_0, x_1) .. i32::max(x_0, x_1) + 1 {
-            canvas.put_pixel(x, y as i32, &color);
+        for x in f64::min(p0.x, p1.x) as i32 .. f64::max(p0.x, p1.x) as i32 + 1 {
+            canvas.put_pixel(x as i32, y as i32, &color);
             y += y_delta;
         }
     } else {
         let x_start;
         let x_delta;
 
-        if y_0 < y_1 {
-            x_start = x_0 as f64;
-            x_delta = (x_1 - x_0) as f64 / y_length as f64;
+        if p0.y < p1.y {
+            x_start = p0.x as f64;
+            x_delta = (p1.x - p0.x) as f64 / y_length as f64;
         } else {
-            x_start = x_1 as f64;
-            x_delta = (x_0 - x_1) as f64 / y_length as f64;
+            x_start = p1.x as f64;
+            x_delta = (p0.x - p1.x) as f64 / y_length as f64;
         }
 
         let mut x = x_start;
 
-        for y in i32::min(y_0, y_1) .. i32::max(y_0, y_1) + 1 {
+        for y in f64::min(p0.y, p1.y) as i32 .. f64::max(p0.y, p1.y) as i32 + 1 {
             canvas.put_pixel(x as i32, y, &color);
             x += x_delta;
         }
@@ -82,19 +99,23 @@ fn draw_line(canvas: &mut Canvas, x_0: i32, y_0: i32, x_1: i32, y_1: i32, color:
 fn main() {
     let mut canvas = Canvas::new("Raster 01 (from chapter 6)", CANVAS_WIDTH, CANVAS_HEIGHT);
 
+    let red = Rgb::from_ints(255,0,0);
+    let blue = Rgb::from_ints(255,0,0);
+    let green = Rgb::from_ints(0,0,255);
+
     // Test data
-    draw_line(&mut canvas, -400,0, 400,0, &Rgb::from_ints(255,0,0));
-    draw_line(&mut canvas, 0,400, 0,-400, &Rgb::from_ints(255,0,0));
+    draw_line(&mut canvas, &Point::from_ints(-400, 0), &Point::from_ints(400, 0), &red);
+    draw_line(&mut canvas, &Point::from_ints(0, 400), &Point::from_ints(0, -400), &red);
 
-    draw_line(&mut canvas, -90,10, 10,110, &Rgb::from_ints(0,255,0));
-    draw_line(&mut canvas, 10,110, 110,10, &Rgb::from_ints(0,255,0));
-    draw_line(&mut canvas, 110,10, 10,-90, &Rgb::from_ints(0,255,0));
-    draw_line(&mut canvas, 10,-90, -90,10, &Rgb::from_ints(0,255,0));
+    draw_line(&mut canvas, &Point::from_ints(-90, 10), &Point::from_ints(10,110), &blue);
+    draw_line(&mut canvas, &Point::from_ints(10, 110), &Point::from_ints(110,10), &blue);
+    draw_line(&mut canvas, &Point::from_ints(110, 10), &Point::from_ints(10,-90), &blue);
+    draw_line(&mut canvas, &Point::from_ints(10, -90), &Point::from_ints(-90,10), &blue);
 
-    draw_line(&mut canvas, -10,-210, -190,-10, &Rgb::from_ints(0,0,255));
-    draw_line(&mut canvas, -190,-10, -10,190, &Rgb::from_ints(0,0,255));
-    draw_line(&mut canvas, -10,190, 190,-10, &Rgb::from_ints(0,0,255));
-    draw_line(&mut canvas, 190,-10, -10,-210, &Rgb::from_ints(0,0,255));
+    draw_line(&mut canvas, &Point::from_ints(-10, -210), &Point::from_ints(-190,-10), &green);
+    draw_line(&mut canvas, &Point::from_ints(-190, -10), &Point::from_ints(-10,190), &green);
+    draw_line(&mut canvas, &Point::from_ints(-10, 190), &Point::from_ints(190,-10), &green);
+    draw_line(&mut canvas, &Point::from_ints(190, -10), &Point::from_ints(-10,-210), &green);
 
     canvas.display_until_exit();
 }
--- examples/raster_02.rs
+++ examples/raster_02.rs
@@ -16,7 +16,6 @@ const CANVAS_WIDTH: usize = 600;
 const CANVAS_HEIGHT: usize = 600;
 
 
-
 /// Iterates over a range of `i32`s, from `i0` to `i1` inclusive, interpolating over a dependent
 /// range of `f64`s, from `d0` to `d1` inclusive.
 ///
@@ -33,11 +32,11 @@ const CANVAS_HEIGHT: usize = 600;
 /// let i = interpolate(10, 500.0, 14, 501.0);
 /// assert_eq!(i, [(10, 500.0), (11, 500.25), (12, 500.5), (13, 500.75), (14, 501.0)]);
 /// ```
-fn interpolate(i0: i32, d0: f64, i1: i32, d1: f64) -> Vec<(i32, f64)> {
+fn interpolate(i0: f64, d0: f64, i1: f64, d1: f64) -> Vec<Point> {
     let mut values = vec![];
 
     if i0 == i1 {
-        values.push((i0, d0));
+        values.push(Point::new(i0, d0));
         return values;
     }
 
@@ -45,17 +44,17 @@ fn interpolate(i0: i32, d0: f64, i1: i32, d1: f64) -> Vec<(i32, f64)> {
     let a;
 
     if i0 < i1 {
-        range = (i0..=i1).into_iter().collect();
-        a = (d1 - d0) / (i1 - i0) as f64;
+        range = (i0 as i32 ..= i1 as i32).into_iter().collect();
+        a = (d1 - d0) / (i1 - i0);
     } else {
-        range = (i1..=i0).rev().into_iter().collect();
-        a = (d1 - d0) / (i0 - i1) as f64;
+        range = (i1 as i32 ..= i0 as i32).rev().into_iter().collect();
+        a = (d1 - d0) / (i0 - i1);
     }
 
     let mut d = d0;
 
     for i in range {
-        values.push((i, d));
+        values.push(Point::new(i as f64, d));
         d = d + a;
     }
 
@@ -63,7 +62,25 @@ fn interpolate(i0: i32, d0: f64, i1: i32, d1: f64) -> Vec<(i32, f64)> {
 }
 
 
-/// Draws a straight line between `x_0`, `y_0` and `x_1`, `y_1` in the given color (inclusive).
+/// A convenience structure to hold a point in 2D space.
+struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Point {
+    #[allow(dead_code)]
+    fn new(x: f64, y: f64) -> Self {
+        Self {x: x, y: y}
+    }
+
+    fn from_ints(x: i32, y: i32) -> Self {
+        Self {x: x as f64, y: y as f64}
+    }
+}
+
+
+/// Draws a straight line between `p0.x`, `p0.y` and `p1.x`, `p1.y` in the given color (inclusive).
 /// Coordinates outside the canvas coordinates do not result in an error and any part of the line
 /// that is within the canvas will be drawn.
 ///
@@ -81,18 +98,17 @@ fn interpolate(i0: i32, d0: f64, i1: i32, d1: f64) -> Vec<(i32, f64)> {
 /// draw_line(&mut canvas, -400,0, 400,0, &Rgb::from_ints(255,0,255));
 /// canvas.display_until_exit();
 /// ```
-fn draw_line(canvas: &mut Canvas, x0: i32, y0: i32, x1: i32, y1: i32, color: &Rgb) {
-
-    let x_length = (x1 - x0).abs();
-    let y_length = (y1 - y0).abs();
+fn draw_line(canvas: &mut Canvas, p0: &Point, p1: &Point, color: &Rgb) {
+    let x_length = (p1.x - p0.x).abs();
+    let y_length = (p1.y - p0.y).abs();
 
     if x_length > y_length {
-        for (x, y) in interpolate(x0, y0 as f64, x1, y1 as f64) {
-            canvas.put_pixel(x, y as i32, &color);
+        for p in interpolate(p0.x, p0.y, p1.x, p1.y) {
+            canvas.put_pixel(p.x as i32, p.y as i32, &color);
         }
     } else {
-        for (y, x) in interpolate(y0, x0 as f64, y1, x1 as f64) {
-            canvas.put_pixel(x as i32, y, &color);
+        for p in interpolate(p0.y, p0.x, p1.y, p1.x) {
+            canvas.put_pixel(p.x as i32, p.y as i32, &color);
         }
     }
 }
@@ -103,19 +119,23 @@ fn main() {
 
     let mut canvas = Canvas::new("Raster 02 (from chapter 6)", CANVAS_WIDTH, CANVAS_HEIGHT);
 
+    let red = Rgb::from_ints(255,0,0);
+    let blue = Rgb::from_ints(255,0,0);
+    let green = Rgb::from_ints(0,0,255);
+
     // Test data
-    draw_line(&mut canvas, -400,0, 400,0, &Rgb::from_ints(255,0,0));
-    draw_line(&mut canvas, 0,400, 0,-400, &Rgb::from_ints(255,0,0));
+    draw_line(&mut canvas, &Point::from_ints(-400, 0), &Point::from_ints(400, 0), &red);
+    draw_line(&mut canvas, &Point::from_ints(0, 400), &Point::from_ints(0, -400), &red);
 
-    draw_line(&mut canvas, -90,10, 10,110, &Rgb::from_ints(0,255,0));
-    draw_line(&mut canvas, 10,110, 110,10, &Rgb::from_ints(0,255,0));
-    draw_line(&mut canvas, 110,10, 10,-90, &Rgb::from_ints(0,255,0));
-    draw_line(&mut canvas, 10,-90, -90,10, &Rgb::from_ints(0,255,0));
+    draw_line(&mut canvas, &Point::from_ints(-90, 10), &Point::from_ints(10,110), &blue);
+    draw_line(&mut canvas, &Point::from_ints(10, 110), &Point::from_ints(110,10), &blue);
+    draw_line(&mut canvas, &Point::from_ints(110, 10), &Point::from_ints(10,-90), &blue);
+    draw_line(&mut canvas, &Point::from_ints(10, -90), &Point::from_ints(-90,10), &blue);
 
-    draw_line(&mut canvas, 10,-210, -190,-10, &Rgb::from_ints(130,130,255));
-    draw_line(&mut canvas, -190,-10, 10,190, &Rgb::from_ints(130,130,255));
-    draw_line(&mut canvas, 10,190, 210,-10, &Rgb::from_ints(130,130,255));
-    draw_line(&mut canvas, 210,-10, 10,-210, &Rgb::from_ints(130,130,255));
+    draw_line(&mut canvas, &Point::from_ints(-10, -210), &Point::from_ints(-190,-10), &green);
+    draw_line(&mut canvas, &Point::from_ints(-190, -10), &Point::from_ints(-10,190), &green);
+    draw_line(&mut canvas, &Point::from_ints(-10, 190), &Point::from_ints(190,-10), &green);
+    draw_line(&mut canvas, &Point::from_ints(190, -10), &Point::from_ints(-10,-210), &green);
 
     canvas.display_until_exit();
 }
