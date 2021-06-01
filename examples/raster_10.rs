76775d28a5c24c09d026da592bcaecb276b257a2
Darius Wiles <github@dariuswiles.com>
2021-06-01 16:37:18 -0700

    Implement more depth buffering logic, but unfinished

--- examples/raster_10.rs
+++ examples/raster_10.rs
@@ -166,13 +166,20 @@ impl DepthBuffer {
     /// required. Otherwise, the stored depth is updated with the value passed and `true` is
     /// returned.
     ///
-    /// # Panics
-    ///
-    /// Panics if `x` is outside the range 0..CANVAS_WIDTH, or `y` is outside the range
-    /// 0..CANVAS_HEIGHT.
+    /// `x` and `y` must be defined in canvas coordinates, e.g., the range of `x` is
+    /// -CANVAS_WIDTH/2..CANVAS_WIDTH/2. If `x` or `y` are outside their respective ranges, no
+    /// changes are made and `false` is returned.
     fn check_set_nearer_pixel(&mut self, x: i32, y: i32, depth: f64) -> bool {
-        if depth < self.buffer[x as usize][y as usize] {
-            self.buffer[x as usize][y as usize] = depth;
+        let screen_x = CANVAS_WIDTH as i32/2 + x;
+        let screen_y = CANVAS_HEIGHT as i32/2 - y - 1;
+
+        if (screen_x < 0) | (screen_x >= CANVAS_WIDTH as i32) | (screen_y < 0) |
+            (screen_y >= CANVAS_HEIGHT as i32) {
+            return false;
+        }
+
+        if depth < self.buffer[screen_x as usize][screen_y as usize] {
+            self.buffer[screen_x as usize][screen_y as usize] = depth;
             return true;
         }
 
@@ -225,66 +232,16 @@ fn interpolate(i0: f64, d0: f64, i1: f64, d1: f64) -> Vec<(f64, f64)> {
 }
 
 
-/// Draws a straight line between `p0.x`, `p0.y` and `p1.x`, `p1.y` in the given color (inclusive).
-/// Coordinates outside the canvas coordinates do not result in an error and any part of the line
-/// that is within the canvas will be drawn.
-///
-/// # Examples
-/// ```
-/// use rust_computer_graphics_from_scratch::canvas::{Canvas, Rgb};
-/// #[allow(unused_imports)]
-/// use crate::vector_math::*;
-/// mod vector_math;
-///
-/// const CANVAS_WIDTH: usize = 600;
-/// const CANVAS_HEIGHT: usize = 600;
-///
-/// let mut canvas = Canvas::new("Raster 01 (from chapter 6)", CANVAS_WIDTH, CANVAS_HEIGHT);
-/// draw_line(&mut canvas, -400,0, 400,0, &Rgb::from_ints(255,0,255));
-/// canvas.display_until_exit();
-/// ```
-fn draw_line(canvas: &mut Canvas, p0: &Point, p1: &Point, color: &Rgb) {
-    let x_length = (p1.x - p0.x).abs();
-    let y_length = (p1.y - p0.y).abs();
-
-    if x_length > y_length {
-        // The line is more horizontal than vertical. Determine which point is more to the left,
-        // so the line is drawn from left to right.
-        let (left, right);
-        if p0.x < p1.x {
-            left = p0;
-            right = p1;
-        } else {
-            left = p1;
-            right = p0;
-        }
-
-        for p in interpolate(left.x, left.y, right.x, right.y) {
-            canvas.put_pixel(p.0.round() as i32, p.1.round() as i32, &color);
-        }
-    } else {
-        // The line is more vertical than horizontal. Determine which point is lower, so the line
-        // is drawn from bottom to top.
-        let (bottom, top);
-        if p0.y < p1.y {
-            bottom = p0;
-            top = p1;
-        } else {
-            bottom = p1;
-            top = p0;
-        }
-        for p in interpolate(bottom.y, bottom.x, top.y, top.x) {
-            canvas.put_pixel(p.1.round() as i32, p.0.round() as i32, &color);
-        }
-    }
-}
-
-
-
 /// Render a wireframe triangle on the canvas, using the viewport coordinates of its corners.
-fn render_triangle(canvas: &mut Canvas, triangle: &Triangle, projected: &Vec<Point>) {
+fn render_triangle(
+    canvas: &mut Canvas,
+    depth_buffer: &mut DepthBuffer,
+    triangle: &Triangle,
+    projected: &Vec<Point>
+) {
     draw_filled_triangle(
         canvas,
+        depth_buffer,
         &projected.get(triangle.vertices.0).unwrap(),
         &projected.get(triangle.vertices.1).unwrap(),
         &projected.get(triangle.vertices.2).unwrap(),
@@ -296,7 +253,14 @@ fn render_triangle(canvas: &mut Canvas, triangle: &Triangle, projected: &Vec<Poi
 /// Draws a filled triangle defined by the three 2D points passed and in the color passed. The
 /// points have already been projected, so their coordinates are in 2D canvas coordinates, i.e.,
 /// -width/2..width/2, and likewise for height.
-fn draw_filled_triangle (canvas: &mut Canvas, p0: &Point, p1: &Point, p2: &Point, color: &Rgb) {
+fn draw_filled_triangle(
+    canvas: &mut Canvas,
+    depth_buffer: &mut DepthBuffer,
+    p0: &Point,
+    p1: &Point,
+    p2: &Point,
+    color: &Rgb
+) {
     let mut corner0 = p0;
     let mut corner1 = p1;
     let mut corner2 = p2;
@@ -337,7 +301,10 @@ fn draw_filled_triangle (canvas: &mut Canvas, p0: &Point, p1: &Point, p2: &Point
         let x_end = x_right.get((y - corner0.y.round() as i32) as usize).unwrap().1.round() as i32;
 
         for x in x_start .. x_end {
-            canvas.put_pixel(x, y, color);
+
+            if depth_buffer.check_set_nearer_pixel(x, y, 10.0) { // TODO replace 10.0 with real `z`
+                canvas.put_pixel(x, y, color);
+            }
         }
     }
 }
@@ -508,7 +475,7 @@ fn transform_and_clip(clipping_planes: &[Plane; 5], model: &Model, transform: &M
 /// Renders the `Model` passed by iterating through the list of triangles and vertices
 /// that it contains, using the `transform` provided to transform each vertex into camera space,
 /// then calling `render_triangle` to draw the triangle on the 2D canvas.
-fn render_instance(canvas: &mut Canvas, model: &Model) {
+fn render_instance(canvas: &mut Canvas, depth_buffer: &mut DepthBuffer, model: &Model) {
     let mut projected = vec![];
 
     // The `vertices` are defined in the 3D world coordinates, so project each vertex onto the
@@ -519,7 +486,7 @@ fn render_instance(canvas: &mut Canvas, model: &Model) {
 
     // Render each triangle by passing coordinates of each corner as a 2D `Point` on the viewport.
     for t in &model.triangles {
-        render_triangle(canvas, &t, &projected);
+        render_triangle(canvas, depth_buffer, &t, &projected);
     }
 }
 
@@ -529,7 +496,11 @@ fn render_instance(canvas: &mut Canvas, model: &Model) {
 /// This is combined with the transform included with each model instance to create a transform
 /// that converts from instance space to camera space. This transform is generated once per
 /// instance and passed as input to the `render_instance` function.
-fn render_scene(canvas: &mut Canvas, camera: &Camera, instances: &[ModelInstance]) {
+fn render_scene(
+    canvas: &mut Canvas,
+    depth_buffer: &mut DepthBuffer,
+    camera: &Camera, instances: &[ModelInstance]
+) {
     let camera_matrix = camera.orientation
         .transpose()
         .multiply_matrix4x4(&Matrix4x4::new_translation_matrix_from_vec4(
@@ -540,7 +511,7 @@ fn render_scene(canvas: &mut Canvas, camera: &Camera, instances: &[ModelInstance
         let transform = camera_matrix.multiply_matrix4x4(&mi.transform);
         let clipped_model = transform_and_clip(&camera.clipping_planes, &mi.model, &transform);
         if let Some(cm) = clipped_model {
-            render_instance(canvas, &cm);
+            render_instance(canvas, depth_buffer, &cm);
         }
     }
 }
@@ -550,6 +521,8 @@ fn render_scene(canvas: &mut Canvas, camera: &Camera, instances: &[ModelInstance
 /// projection using rasterization techniques.
 fn main() {
     let mut canvas = Canvas::new("Raster 10 (from chapter 12)", CANVAS_WIDTH, CANVAS_HEIGHT);
+    let mut depth_buffer = DepthBuffer::new();
+
 
     let red = Rgb::from_ints(255,0,0);
     let green = Rgb::from_ints(0,255,0);
@@ -638,7 +611,7 @@ fn main() {
                     ],
                 };
 
-    render_scene(&mut canvas, &camera, &instances);
+    render_scene(&mut canvas, &mut depth_buffer, &camera, &instances);
 
     canvas.display_until_exit();
 }
